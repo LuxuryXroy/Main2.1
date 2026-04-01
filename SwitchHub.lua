046208a682f354e63689243c22fd968535c1f1c0
LuxuryXroy <leminhthatssa13@gmail.com>
2026-04-01 18:43:13 +0700

    SwitchHub.lua

--- SwitchHub.lua
+++ SwitchHub.lua
@@ -36,7 +36,6 @@ ToggleButton.TextSize = 14
 
 -- THIẾT LẬP MÀU SẮC THEO YÊU CẦU
 ToggleButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255) -- Màu trắng
-ToggleButton.BackgroundTransparency = 0.4 -- Trong suốt khoảng 40% (Hiện 60%)
 ToggleButton.TextColor3 = Color3.fromRGB(0, 0, 0) -- Chữ màu đen
 ToggleButton.BorderSizePixel = 0
 
@@ -59,79 +58,3 @@ ToggleButton.MouseButton1Click:Connect(function()
     else
         ToggleButton.Text = "[ Switch Hub🇻🇳 | Blox Fruits ]\nFast Attack: OFF"
     end
-end)
-
---=========================================
--- CORE SCRIPT FAST ATTACK
---=========================================
-local v1 = next
-local v2 = {
-    game:GetService("ReplicatedStorage").Util,
-    game:GetService("ReplicatedStorage").Common,
-    game:GetService("ReplicatedStorage").Remotes,
-    game:GetService("ReplicatedStorage").Assets,
-    game:GetService("ReplicatedStorage").FX,
-}
-local v3, u4, u5 = nil, nil, nil
-
-task.spawn(function()
-    while true do
-        local v6
-        v3, v6 = v1(v2, v3)
-        if v3 == nil then break end
-        for _, v10 in pairs(v6:GetChildren()) do
-            if v10:IsA('RemoteEvent') and v10:GetAttribute('Id') then
-                u5 = v10:GetAttribute('Id')
-                u4 = v10
-            end
-        end
-    end
-end)
-
-task.spawn(function()
-    while task.wait(0.0001) do
-        if _G.AutoAttack then
-            local _Character = LocalPlayer.Character
-            local v13 = _Character and _Character:FindFirstChild('HumanoidRootPart')
-
-            if v13 then
-                local u17 = {}
-                local targets = {workspace.Enemies, workspace.Characters}
-                
-                for _, folder in pairs(targets) do
-                    for _, v22 in pairs(folder:GetChildren()) do
-                        local _Humanoid = v22:FindFirstChild('Humanoid')
-                        local _HRP = v22:FindFirstChild('HumanoidRootPart')
-                        
-                        if v22 ~= _Character and _HRP and _Humanoid and _Humanoid.Health > 0 then
-                            local dist = (_HRP.Position - v13.Position).Magnitude
-                            if dist <= 60 then
-                                for _, part in pairs(v22:GetChildren()) do
-                                    if part:IsA('BasePart') then
-                                        table.insert(u17, {v22, part})
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-
-                local _Tool = _Character:FindFirstChildOfClass('Tool')
-                if #u17 > 0 and (_Tool and (_Tool:GetAttribute('WeaponType') == 'Melee' or _Tool:GetAttribute('WeaponType') == 'Sword')) then
-                    pcall(function()
-                        require(game.ReplicatedStorage.Modules.Net):RemoteEvent('RegisterHit', true)
-                        game.ReplicatedStorage.Modules.Net['RE/RegisterAttack']:FireServer()
-                        
-                        local _Head = u17[1][1]:FindFirstChild('Head')
-                        if _Head and u4 and u5 then
-                            game.ReplicatedStorage.Modules.Net['RE/RegisterHit']:FireServer(_Head, u17, {}, tostring(LocalPlayer.UserId):sub(2, 4) .. tostring(coroutine.running()):sub(11, 15))
-                            u4:FireServer(string.gsub('RE/RegisterHit', '.', function(p31)
-                                return string.char(bit32.bxor(string.byte(p31), math.floor(workspace:GetServerTimeNow() / 10 % 10) + 1))
-                            end), bit32.bxor(u5 + 909090, game.ReplicatedStorage.Modules.Net.seed:InvokeServer() * 2), _Head, u17)
-                        end
-                    end)
-                end
-            end
-        end
-    end
-end)
